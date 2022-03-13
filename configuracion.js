00edd30c0fef67627a8df2de4a339d4e164cd98f
Felipe Miranda <l_felipe-mv97@hotmail.com>
2022-03-12 22:24:03 -0500

    Exportar Origen

---
+++ configuracion.js
@@ -0,0 +1,5 @@
+const config = {
+    DB_URL: 'mongodb+srv://luisfelipe:ups50@clustersalesiana.sizha.mongodb.net/tiendaHeroku?retryWrites=true&w=majority',
+    PUERTO: process.env.PORT || 3000
+}
+module.exports = config
