68ab3aa8983618097c824228daab18cf65f354cb
Felipe Miranda <l_felipe-mv97@hotmail.com>
2022-03-12 22:54:30 -0500

    m

--- configuracion.js
+++ configuracion.js
@@ -1,5 +1,5 @@
 const config = {
     DB_URL: 'mongodb+srv://luisfelipe:ups50@clustersalesiana.sizha.mongodb.net/tiendaHeroku?retryWrites=true&w=majority',
-    PUERTO: process.env.PORT || 3000
+    PUERTO: process.env.PORT || 6000
 }
 module.exports = config
